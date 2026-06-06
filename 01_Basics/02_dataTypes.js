42bc863f1589d85632c1df32aba947f7d4382fc1
Dinesh Suwalkya <155345746+Suwalkya-ji@users.noreply.github.com>
2026-06-06 12:31:23 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,44 @@
+"use strict"; // use for new javascript version and block old javascripts -> treat all js code as newer version
+
+// alert(3+3)   // we are using node.js, not browser
+
+// code readability should be high
+console.log( 3
+    +
+    3
+);
+
+console.log("Dinesh");
+
+// Documentation 
+
+// 1. standards and oficial documentation
+let standards = "https://tc39.es/ecma262/multipage/#sec-intro";
+
+// 2. easy documentation 
+let MDN = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+
+
+//  Data Types 
+
+let name = "dinesh" //string
+let id = 233 //number
+let isLogIn = true //boolean
+let state;
+
+// Primitive Data types
+
+// Number -> range (2^53)
+// bigint -> big numbers
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined 
+// symbol -> for find unique
+// object
+
+
+// find typeof data
+console.log(typeof "dinesh") //-> string
+console.log(typeof null);  // -> object
+console.log(typeof undefined) // -> undefined
